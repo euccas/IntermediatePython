c8253350436da3c3f9cb5f95668c39ff088bd6de
euccas <euccas.chen@gmail.com>
2016-09-20 22:10:39 -0700

    Add error raising

--- class/LogRadiusChanges.py
+++ class/LogRadiusChanges.py
@@ -15,6 +15,8 @@
 # >>> circle.radius = 3
 # >>> circle.radius_changes
 # [1, 2, 3]
+#
+# Take our Circle class and modify it again to raise a ValueError when the radius or diameter are set to a negative number.
 
 import math
 
@@ -29,5 +31,7 @@ class Circle:
 
     @radius.setter
     def radius(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
         self._radius = radius
         self.radius_changes.append(self._radius)
