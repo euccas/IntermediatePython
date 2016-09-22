d40ae5a00397029f06aac7c569cc9187827a3f1a
euccas <euccas.chen@gmail.com>
2016-09-22 10:53:03 -0700

    Update flattern matrix

--- comprehension/FlattenMatrix.py
+++ comprehension/FlattenMatrix.py
@@ -1,3 +1,12 @@
+# Flatten a Matrix
+# Create a function flatten, that will take a matrix (a list of lists) and return a flattened version of the matrix.
+#
+# >>> matrix = [[row * 3 + incr for incr in range(1, 4)] for row in range(4)]
+# >>> matrix
+# [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+# >>> flatten(matrix)
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 def flatten(matrix):
     flattened = []
     for row in matrix:
