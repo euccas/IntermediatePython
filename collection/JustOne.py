b25e994e56debba88c23d9a4e055a6d3bd37ea3d
euccas <euccas.chen@gmail.com>
2016-09-23 10:51:29 -0700

    Add exercise JustOne

---
+++ collection/JustOne.py
@@ -0,0 +1,28 @@
+class JustOne:
+
+    def __contains__(self, value):
+    # optional
+        if value == 1:
+            return True
+        return False
+        # return (value == 1)
+
+    def __len__(self):
+        return 1
+
+    def __iter__(self):
+        return iter([1])
+
+    #def __iter__(self):
+    #    yield 1
+
+    def __next__(self):
+    # optional
+        return 1
+
+    def __getitem__(self, i):
+        return 1
+
+    def __bool__(self):
+    # optional
+        return True
