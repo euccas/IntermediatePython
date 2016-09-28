8b7443b9ac25055d07b6a4003028115dfc3d0170
euccas <euccas.chen@gmail.com>
2016-09-28 10:47:53 -0700

    Update Total Length

--- itertools/TotalLength.py
+++ itertools/TotalLength.py
@@ -4,10 +4,10 @@ from itertools import *
 
 def total_length(*iterables):
     #chain(*iterables)
+
     #return len(list(chain.from_iterable(iterables)))
 
-    return sum(1 for _ in chain.from_iterable(iterables))
-
+    return sum(1 for _ in chain.from_iterable(iterables)) # Not storing the list in memory, could be more efficient
 
 if __name__ == "__main__":
     print(total_length([1,2,3], [4,5], iter([6, 7])))
