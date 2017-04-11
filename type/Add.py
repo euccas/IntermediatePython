6bc74e2a67de491354ab24848913ff3c9054a001
euccas <euccas.chen@gmail.com>
2017-04-12 00:17:48 +0800

    add Add.py

---
+++ type/Add.py
@@ -0,0 +1,10 @@
+# Add
+## Make a program that takes two numbers
+## and prints out the sum of these numbers
+
+def add(a, b):
+    return a + b
+
+print(add(3, 5.2))
+print(add(-7, 2))
+print(add(-2, -1))
